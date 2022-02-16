d13be4f8608c7356584d70b85cb856e74f54119e
Laurent Gatto <laurent.gatto@uclouvain.be>
2022-02-16 17:45:39 +0100

    add clusting

--- figs/code.R
+++ figs/code.R
@@ -4,6 +4,8 @@ x <- data.frame(black = c(10, 11, 0, 123, 2),
                 steelblue = c(3, 0, 3, 2, 23))
 rownames(x) <- c("main", "pince", "tete",
                  "jambes", "corps")
+xfr <- x
+colnames(xfr) <- c("noir", "jaune", "mauve", "bleu")
 
 png("pca1.png")
 pairs(t(x), col = colnames(x), pch = 19, cex = 3)
@@ -25,4 +27,17 @@ fviz_pca_ind(pca, habillage = colnames(x),
         values = cls) +
     scale_shape_manual(values = rep(19, 4)) +
     theme(legend.position = "none")
+dev.off()
+
+library(dendextend)
+
+knitr::kable(as.matrix(dist(t(xfr))))
+
+png("hc.png")
+hc <- hclust(dist(t(xfr)))
+hc <- as.dendrogram(hc) |>
+    set("labels_colors", colnames(x)) |>
+    set("branches_lwd", 1) |>
+    set("labels_cex", 2)
+plot(hc)
 dev.off()
